f8bd3713dc4530cacb4d5ec281d21b2e0ac0cb6c
Chenxin <chenxinyang2022@u.northwestern.edu>
2022-05-04 22:42:17 -0500

    s3
    add raw data, s3.py, run_s3.py

--- run_s3.py
+++ run_s3.py
@@ -1,22 +1,33 @@
 import argparse
 import logging.config
 
-from src.s3 import download_file_from_s3, upload_file_to_s3
+from src.s3 import download_file_from_s3, upload_file_to_s3, download_from_s3_pandas, upload_to_s3_pandas
 
 logging.config.fileConfig('config/logging/local.conf')
 logger = logging.getLogger('s3-pipeline')
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
+    parser.add_argument('--sep',
+                        default=';',
+                        help="CSV separator if using pandas")
+    parser.add_argument('--pandas', default=False, action='store_true',
+                        help="If used, will load data via pandas")
     parser.add_argument('--download', default=False, action='store_true',
-                        help="If True, will download the data from S3. If False, will upload data to S3")
-    parser.add_argument('--s3_path', default='s3://2021-msia423-wenyang-pan/raw/pokemon.csv',
-                        help="s3 data path to download or upload data")
-    parser.add_argument('--local_path', default='data/sample/pokemon.csv',
-                        help="local data path to store or upload data")
+                        help="If used, will load data via pandas")
+    parser.add_argument('--s3path', default='s3://2022-msia423-yang-chenxin/raw_data/employee_train.csv',
+                        help="If used, will load data via pandas")
+    parser.add_argument('--local_path', default='data/raw/employee_attrition_train.csv',
+                        help="Where to load data to in S3")
     args = parser.parse_args()
 
     if args.download:
-        download_file_from_s3(args.local_path, args.s3_path)
+        if args.pandas:
+            download_from_s3_pandas(args.local_path, args.s3path, args.sep)
+        else:
+            download_file_from_s3(args.local_path, args.s3path)
     else:
-        upload_file_to_s3(args.local_path, args.s3_path)
+        if args.pandas:
+            upload_to_s3_pandas(args.local_path, args.s3path, args.sep)
+        else:
+            upload_file_to_s3(args.local_path, args.s3path)
--- src/s3.py
+++ src/s3.py
@@ -7,21 +7,12 @@ import boto3
 import botocore
 import pandas as pd
 
-# logging.config.fileConfig('config/logging.conf')
-# logger = logging.getLogger('s3.py-demo')
+logging.config.fileConfig('config/logging/local.conf')
+logger = logging.getLogger('s3-demo')
 
-logger = logging.getLogger(__name__)
 
 def parse_s3(s3path: str) -> typing.Tuple[str, str]:
-    """
-    Parse the s3 path to get the bucket name and the path name
-    Args:
-        s3path (str): the input s3 path
-    Returns:
-        s3bucket (str): the s3 bucket name
-        s3path (str): the s3 path
-    """
-    regex = r"s3.py://([\w._-]+)/([\w./_-]+)"
+    regex = r"s3://([\w._-]+)/([\w./_-]+)"
 
     m = re.match(regex, s3path)
     s3bucket = m.group(1)
@@ -31,41 +22,34 @@ def parse_s3(s3path: str) -> typing.Tuple[str, str]:
 
 
 def upload_file_to_s3(local_path: str, s3path: str) -> None:
-    """
-    Upload the file in local path to s3
-    Args:
-        local_path (str): the path that points to the local data
-        s3path (str): the s3 path that the data will be uploaded to
-    Returns: None
-    """
-
     s3bucket, s3_just_path = parse_s3(s3path)
 
-    s3 = boto3.resource("s3.py")
+    s3 = boto3.resource("s3")
     bucket = s3.Bucket(s3bucket)
 
     try:
         bucket.upload_file(local_path, s3_just_path)
     except botocore.exceptions.NoCredentialsError:
         logger.error('Please provide AWS credentials via AWS_ACCESS_KEY_ID and AWS_SECRET_ACCESS_KEY env variables.')
-    except FileNotFoundError:
-        logger.error('Please check whether your local path contains the data you want to upload', exc_info=True)
     else:
         logger.info('Data uploaded from %s to %s', local_path, s3path)
 
 
+def upload_to_s3_pandas(local_path: str, s3path: str, sep: str = ';') -> None:
+    df = pd.read_csv(local_path, sep=sep)
+
+    try:
+        df.to_csv(s3path, sep=sep)
+    except botocore.exceptions.NoCredentialsError:
+        logger.error('Please provide AWS credentials via AWS_ACCESS_KEY_ID and AWS_SECRET_ACCESS_KEY env variables.')
+    else:
+        logger.info('Data uploaded from %s to %s', local_path, s3path)
+
 
 def download_file_from_s3(local_path: str, s3path: str) -> None:
-    """
-        Download a data file from s3
-        Args:
-            local_path (str): the path that will store the downloaded data
-            s3path (str): the s3 path that the data will be downloaded from
-        Returns: None
-    """
     s3bucket, s3_just_path = parse_s3(s3path)
 
-    s3 = boto3.resource("s3.py")
+    s3 = boto3.resource("s3")
     bucket = s3.Bucket(s3bucket)
 
     try:
@@ -74,3 +58,13 @@ def download_file_from_s3(local_path: str, s3path: str) -> None:
         logger.error('Please provide AWS credentials via AWS_ACCESS_KEY_ID and AWS_SECRET_ACCESS_KEY env variables.')
     else:
         logger.info('Data downloaded from %s to %s', s3path, local_path)
+
+
+def download_from_s3_pandas(local_path: str, s3path: str, sep: str = ';'):
+    try:
+        df = pd.read_csv(s3path, sep=sep)
+    except botocore.exceptions.NoCredentialsError:
+        logger.error('Please provide AWS credentials via AWS_ACCESS_KEY_ID and AWS_SECRET_ACCESS_KEY env variables.')
+    else:
+        df.to_csv(local_path, sep=sep)
+        logger.info('Data uploaded from %s to %s', local_path, s3path)
